2efec9b90ae6edffa4283380f88e08d66ecf179c
srinivasarao <kunchala1971@gmail.com>
2025-10-14 09:56:50 +0530

    update

--- 15.MultiThreading/Thread5.py
+++ 15.MultiThreading/Thread5.py
@@ -1,8 +1,8 @@
 #set the thread name and get the thread name
 from threading import *
-#print(current_thread().getName())
+# print(current_thread().getName())
 print(current_thread().name)
-#current_thread().setName("KKCC INFO SYSTEMS")
+# current_thread().setName("KKCC INFO SYSTEMS")
 current_thread().name="KKCC INFO SYSTEMS"
-#print(current_thread().getName())
+# print(current_thread().getName())
 print(current_thread().name)
--- 15.MultiThreading/Thread8.py
+++ 15.MultiThreading/Thread8.py
@@ -2,7 +2,7 @@ from threading import *
 import time
 def display():
     print("\n"+ current_thread().name,"...started\n")
-    time.sleep(3)
+    time.sleep(4)
     print("\n"+ current_thread().name,"...ended\n")
 
 t1=Thread(target=display,name="ChildThread1")
